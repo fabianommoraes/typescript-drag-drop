b2921e3051364867af2ac605c2b6a0b816857465
fabianommoraes <fabiano.m.moraes@gmail.com>
2024-01-30 17:02:28 -0300

    127. Rendering Project Lists

--- src/app.js
+++ src/app.js
@@ -44,6 +44,29 @@ function autobind(_, _2, descriptor) {
     };
     return adjDescriptor;
 }
+// ProjectList Class
+var ProjectList = /** @class */ (function () {
+    function ProjectList(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
+        var importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = "".concat(this.type, "-projects");
+        this.attach();
+        this.renderContent();
+    }
+    ProjectList.prototype.renderContent = function () {
+        var listId = "".concat(this.type, "-projects-list");
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent =
+            this.type.toUpperCase() + ' PROJECTS';
+    };
+    ProjectList.prototype.attach = function () {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    };
+    return ProjectList;
+}());
 // ProjectInput Class
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
@@ -116,3 +139,5 @@ var ProjectInput = /** @class */ (function () {
     return ProjectInput;
 }());
 var prjInput = new ProjectInput();
+var activePrjList = new ProjectList('active');
+var finishedPrjList = new ProjectList('finished');
